ac90b7c789add2b2c01e859bcb969bc6ed7822cd
AdamNewmanBooth <164922100+AdamNewmanBooth@users.noreply.github.com>
2024-04-24 20:05:15 +0000

    Commit5

--- app/controllers/zebra_controller.rb
+++ app/controllers/zebra_controller.rb
@@ -5,14 +5,20 @@ class ZebraController <ApplicationController
   end
 
   def rock
+    @random_move = ["Rock", "Paper", "Scissors"].sample
     render({:template => "game_template/play_rock"})
+   
   end
 
 def paper
+  @random_move = ["Rock", "Paper", "Scissors"].sample
   render({:template => "game_template/play_paper"})
+  
 end
 
 def scissors
+  @random_move = ["Rock", "Paper", "Scissors"].sample
   render({:template => "game_template/play_scissors"})
+ 
 end
 end
